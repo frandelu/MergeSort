e83e8e01c4df7e9c381d7b0613b938037216adf4
Fran <fdelucaa@fi.uba.ar>
2022-11-02 17:55:07 -0300

    crear

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/frandelu/MergeSort.git
+
+go 1.18
---
+++ mergesort.go
@@ -0,0 +1,31 @@
+package cola_prioridad
+
+func MergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	medio := len(arr) / 2
+	return merge(MergeSort(arr[:medio]), MergeSort(arr[medio:]))
+}
+
+func merge(izq, der []int) []int {
+	largo := len(izq) + len(der)
+	arr := make([]int, largo)
+	i, j := 0, 0
+	for k := 0; k < largo; k++ {
+		if i > len(izq)-1 && j <= len(der)-1 {
+			arr[k] = der[j]
+			j++
+		} else if j > len(der)-1 && i <= len(izq)-1 {
+			arr[k] = izq[i]
+			i++
+		} else if izq[i] < der[j] {
+			arr[k] = izq[i]
+			i++
+		} else {
+			arr[k] = der[j]
+			j++
+		}
+	}
+	return arr
+}
